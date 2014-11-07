30d8e0802f7803d1b5715673f2457a04e3bfcad5
ovi ovi <craiu.ovidiu93@yahoo.com>
2014-11-07 15:11:53 +0200

    a

---
+++ src/main/java/ro/teamnet/z2h/web/ZeroToHeroServlet.java
@@ -0,0 +1,29 @@
+package ro.teamnet.z2h.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by ovy on 11/7/2014.
+ */
+public class ZeroToHeroServlet extends HttpServlet {
+
+    private String handleRequest(HttpServletRequest req){
+        String c=req.getParameter("first");
+        String response="Hello <b>["+c+"] ["+req.getParameter("last")+"]</b>! Enjoy Zero To Hero!!!";
+        return response;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+
+        resp.getWriter().write(handleRequest(req));
+
+
+    }
+}
